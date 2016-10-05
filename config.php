2cd15a269f498822e093658223dcc310b95a165d
brayan <brayan.ideasyproyectos@gmail.com>
2016-10-04 19:02:26 -0500

    registro de usuarios

--- config.php
+++ config.php
@@ -1,9 +1,17 @@
 <?php
 	session_start();
 	//db constants
+	/*
 	define( "DB_DSN", "mysql:localhost;dbname=u913897248_adic" );
 	define( "DB_USERNAME", "root" );
 	define( "DB_PASSWORD", "root" );
+	define( "DB_HOST", "localhost" );
+	define( "DB_NAME", "u913897248_adic");*/
+
+
+	define( "DB_DSN", "mysql:localhost;dbname=u913897248_adic" );
+	define( "DB_USERNAME", "u983574901_aadic" );
+	define( "DB_PASSWORD", "u913897248_adicDB" );
 	define( "DB_HOST", "localhost" );
 	define( "DB_NAME", "u913897248_adic");
 
