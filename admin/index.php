c9621faea2c71f1dd2dad77cf5e5f0bdc427f46b
juan carlos <jcmedinamartinez@gmail.com>
2016-11-06 23:32:49 -0600

    Filtrado por dias

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,27 @@
-<html lang="en-US">
+
+<!DOCTYPE html>
+<html>
 <head>
-  <meta charset="utf-8">
+ <meta charset="utf-8">
     <title>Login</title>
     
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700">
     <link rel="stylesheet" href="css/login.css">
 
+  <title></title>
 </head>
-    <div id="login">
-      <form name='form-login'>
-        <span class="fontawesome-user"></span>
-          <input type="text" id="user" placeholder="Username">
-       
-        <span class="fontawesome-lock"></span>
-          <input type="password" id"pass" placeholder="Password">
-        
-        <input type="submit" value="Login">
+<body>
+  <div id="login">
+    <form name='form-login'>
+            <span class="fontawesome-user"></span>
+              <input type="text" id="user" placeholder="Username">
+           
+            <span class="fontawesome-lock"></span>
+              <input type="password" id"pass" placeholder="Password">
+            
+            <input id="loginAdmin" type="button" value="Login">
 
-</form>
+    </form>
+  </div>
+</body>
+</html>
