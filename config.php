fb252106f31375d944a4cedfe271631675b6f7aa
Juan Carlos Medina <jcmedinamartinez@gmail.com>
2016-11-21 23:36:12 -0600

    x

--- config.php
+++ config.php
@@ -10,7 +10,7 @@
 
 
 	define( "DB_DSN", "mysql:aav1dgv9gsq87g.cwhaggvv5lvi.us-west-2.rds.amazonaws.com;dbname=u913897248_adic" );
-	define( "DB_USERNAME", "webmaster" );
+	define( "DB_USERNAME", "root" );
 	define( "DB_PASSWORD", "N3utralz0n3" );
 	define( "DB_HOST", "aav1dgv9gsq87g.cwhaggvv5lvi.us-west-2.rds.amazonaws.com" );
 	define( "DB_NAME", "u913897248_adic");
