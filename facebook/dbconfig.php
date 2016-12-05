76f598922dadc3c335578ce48edf1ce33dfdb80b
Booz Avila <raganaxi@gmail.com>
2016-12-05 13:28:08 -0600

    ok

--- facebook/dbconfig.php
+++ facebook/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_SERVER', 'aav1dgv9gsq87g.cwhaggvv5lvi.us-west-2.rds.amazonaws.com');
-define('DB_USERNAME', 'webmaster');    // DB username
-define('DB_PASSWORD', 'N3utralz0n3');    // DB password
-define('DB_DATABASE', 'u913897248_adic');      // DB name
+define('DB_USERNAME', 'webmaster');
+define('DB_PASSWORD', 'N3utralz0n3');
+define('DB_DATABASE', 'u913897248_adic');
 $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
 $database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
 date_default_timezone_set('America/Mexico_City');
