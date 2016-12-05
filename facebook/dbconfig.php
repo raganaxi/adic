45814aaa0e74842593e71eec782820fba53d5a16
booz <raganaxi@gmail.com>
2016-12-05 05:07:46 -0600

    tratando de hacerlo por php7

--- facebook/dbconfig.php
+++ facebook/dbconfig.php
@@ -1,8 +1,10 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'username');    // DB username
-define('DB_PASSWORD', 'password');    // DB password
-define('DB_DATABASE', 'database');      // DB name
+
+define('DB_SERVER', 'aav1dgv9gsq87g.cwhaggvv5lvi.us-west-2.rds.amazonaws.com');
+define('DB_USERNAME', 'webmaster');    // DB username
+define('DB_PASSWORD', 'N3utralz0n3');    // DB password
+define('DB_DATABASE', 'u913897248_adic');      // DB name
 $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
 $database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
+date_default_timezone_set('America/Mexico_City');
 ?>
