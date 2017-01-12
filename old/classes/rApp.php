5bed0b1a3bbb1393c12ae2a041724a0c2ac964cc
Booz Avila <raganaxi@gmail.com>
2017-01-12 00:47:11 -0600

    depurando api2

--- old/classes/rApp.php
+++ old/classes/rApp.php
@@ -23,7 +23,20 @@ $action="";
 
 if (is_ajax()){
 	if ($action!="") {
-		
+		switch($action) { /*//Switch case for value of action*/
+			case 'sesion': sesion_function();break;
+			case 'loginU': login_function(); break;
+			case 'logout': logout_function();break;
+			case 'registerU': register_user();break;
+			case 'getPost': getPost_function();break;
+			case 'getPostSocio': getPostSocio_function();break;
+			case 'getNegocios': getNegocios_function();break;
+			case 'getAddress': getAddress_function();break;
+			case 'getCat': getCat_function();break;
+			case 'activateT': activateToken_function();break;
+			case 'info' : info_function();break;
+
+		}
 
 	}else{
 		$continuar="no_ok";
@@ -36,7 +49,141 @@ if (is_ajax()){
 function is_ajax() {
 	return true;/*isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';*/
 }
+function info_function(){
+	global $db_con;
+	global $continuar;
+	global $error;
+	global $datos;
+	global $mensaje;
+	$result = posts::info_info();
+	if (!empty($result)) {
+		$continuar ="ok";
+		$datos=$result;		
+	}
+	else{
+		$continuar="no_ok";
+		$error="no_ok";
+		$mensaje="no mms"; /* wrong details */
+	}		
+}
+function register_user(){
+	global $db_con;
+	global $continuar;
+	global $error;
+	global $datos;
+	global $mensaje;
+	$logUser="";
+	$logPass="";	
+	switch($_SERVER['REQUEST_METHOD'])
+	{
+		case 'GET':
 
+		if (isset($_GET["mail"]) && !empty($_GET["mail"])) {
+			$logUser=$_GET["mail"];
+		}
+		if (isset($_GET["pass"]) && !empty($_GET["pass"])) {
+			$logPass=$_GET["pass"];
+		}
+		break;
+		case 'POST':		
+		if (isset($_POST["mail"]) && !empty($_POST["mail"])) {
+			$logUser=$_POST["mail"];
+		}
+		if (isset($_POST["pass"]) && !empty($_POST["pass"])) {
+			$logPass=$_POST["pass"];
+		}
+		break;
+		default:
+	}	
+	if($logUser!=""){
+		$logUser = trim($logUser);
+		$logPass = trim($logPass);
+		$register_result = user::register($logUser, $logPass, "email");
+		if ($register_result=="Y") {
+			$result = user::login($logUser, $logPass);
+			if (!empty($result)) {
+				$continuar ="ok"; /*login on*/
+				$datos['row']=$result;
+				$newToken=	user::obtenToken512($logUser,$result[0]['iduser'],"localhost","prueba");
+				if($newToken){
+					$datos['token']=$newToken;
+				}	
+			}
+			else{
+				$continuar="no_ok";
+				$error="no_error";
+				$mensaje="email o contraseña no existen "; /* wrong details */
+			}		
+		}else{
+			$continuar="no_ok";
+			$error="no_ok";
+			$mensaje="usario/correo ya registrado"; /* wrong details */
+		}
+
+
+	}
+	else{
+		$continuar="no_ok";
+		$error="no_error";
+		$mensaje="favor de revisar los campos requeridos";
+
+	}
+}
+function login_function(){
+	global $db_con;
+	global $continuar;
+	global $error;
+	global $datos;
+	global $mensaje;
+	$logUser="";
+	$logPass="";	
+	switch($_SERVER['REQUEST_METHOD'])
+	{
+		case 'GET':
+
+		if (isset($_GET["logUser"]) && !empty($_GET["logUser"])) {
+			$logUser=$_GET["logUser"];
+		}
+		if (isset($_GET["logPass"]) && !empty($_GET["logPass"])) {
+			$logPass=$_GET["logPass"];
+		}
+		break;
+		case 'POST':		
+		if (isset($_POST["logUser"]) && !empty($_POST["logUser"])) {
+			$logUser=$_POST["logUser"];
+		}
+		if (isset($_POST["logPass"]) && !empty($_POST["logPass"])) {
+			$logPass=$_POST["logPass"];
+		}
+		break;
+		default:
+	}
+	if($logUser!=""){
+		$logUser = trim($logUser);
+		$logPass = trim($logPass);
+		//$logPass = md5($logPass);
+		$result = user::login($logUser, $logPass);
+		if (!empty($result)) {
+			$continuar ="ok"; /*login on*/
+			$datos['row']=$result;
+			$newToken=	user::obtenToken512($logUser,$result[0]['iduser'],"localhost","prueba");
+			if($newToken){
+				$datos['token']=$newToken;
+			}	
+		}
+		else{
+			$continuar="no_ok";
+			$error="no_ok";
+			$mensaje="email o contraseña no existen "; /* wrong details */
+		}		
+	}
+	else{
+		$continuar="no_ok";
+		$error="no_error";
+		$mensaje="favor de revisar los campos requeridos";
+
+	}
+}
 
 
 ?>
