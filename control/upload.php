9abf0d95c845b3e3768663afaedc58b0251dbea7
Booz Avila <raganaxi@gmail.com>
2017-01-22 23:40:12 -0600

    cambios 3

--- control/upload.php
+++ control/upload.php
@@ -52,7 +52,7 @@ function profile_function(){
 
 function update_profile_function(){
 	global $iduserXXX;
-	/*
+	
 	try{
 		$directory = "../imagenes_/profPicture/";
 		/*if (!file_exists($directory)) {
@@ -60,19 +60,21 @@ function update_profile_function(){
 		}
 		if (!file_exists($directory.$iduserXXX)) {
 			mkdir($directory.$iduserXXX, 0777,true);
-		}*
+		}*/
+
+		
 		$nombreArchivo=isset($_FILES['fileImage']['name']) ?$_FILES['fileImage']['name']: null;
 		$nombreTemporal=isset($_FILES['fileImage']['tmp_name'])?$_FILES['fileImage']['tmp_name']:null;
 
 		$nombreArchivo=$iduserXXX."_".time()."_".str_replace(" ", "_", $nombreArchivo);
 
 		$rutaArchivo=$directory.$nombreArchivo;
-
+		/*
 		//move_uploaded_file($nombreTemporal,$rutaArchivo);
 
 		$img=$nombreArchivo;
 		$result = user::updateProfPicture($img,$iduserXXX);
-		echo json_encode($result);
+		echo json_encode($result);*/
 	}
 	catch(Exception $e){
 		error_log($e);
@@ -81,7 +83,7 @@ function update_profile_function(){
 	finally{
 		return;
 		die;
-	}*/
+	}
 }
 /*
 function create_post_function(){
